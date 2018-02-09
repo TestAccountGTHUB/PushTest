291e724e04cfeac1e36abcd04a9eb7f4231757d0
TestAccountGTHUB <34683998+TestAccountGTHUB@users.noreply.github.com>
2018-02-09 16:57:51 +0530

    Push.sh file commit

---
+++ push.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "trythisemail123@gmail.com"
+  git config --global user.name "TestAccountGTHUB"
+}
+
+commit_website_files() { 
+  git add src/test/resources/*
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip CI]"
+}
+
+upload_files() {
+  git push
+}
+
+setup_git
+commit_website_files
+upload_files
